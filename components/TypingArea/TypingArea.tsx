798b378613783362724bf5c1fedd4cca2934f330
Pawel <pawel.bornikowski@gmail.com>
2023-12-30 02:49:08 +0100

    chore: read and create same arrays
    create same dim array of clicked keys and orgrinal text to compare

--- components/TypingArea/TypingArea.tsx
+++ components/TypingArea/TypingArea.tsx
@@ -8,13 +8,13 @@ import { Context } from "@/context/StateContext";
 
 type TypedValue = {
   char: string;
-  index: number;
+
 };
 
 type TypedValues = {
-  correct: TypedValue[];
-  wrong: TypedValue[];
-  current: TypedValue[];
+  correct: string[];
+  wrong:  string[];
+  current:  string[];
 };
 
 const TypingArea = () => {
@@ -28,43 +28,44 @@ const TypingArea = () => {
     }
   };
 
-
-
-
   const [typedValues, setTypedValues] = useState<TypedValues>({
     correct: [],
     wrong: [],
     current: [],
   });
 
-
   const handleTyping = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const orginal = textContent.split("");
-    const typed = e.target.value
-
-    
-    const slicedOrginal = orginal.slice(0, typedValues.current.length+1);
- 
-
- console.log(slicedOrginal)
-
-   setTypedValues((prev) => {
-    const index = prev.current.length; // Get the next index
-    const newTypedValue = { char: typed, index }; // Create a new TypedValue object
+    const typed = e.target.value;
+    const typedSplit = typed.split("");
+    // console.log(typed.length)
+    console.log(typedSplit[typed.length-1])
   
-    return {
-      ...prev,
-      current: [
-        ...prev.current,
-        newTypedValue
-      ],
-    };
-  });
+
+    setTypedValues((prev) => {
+      const index = prev.current.length; // Get the next index
+      const newTypedValue = typedSplit[typed.length-1]; // Create a new TypedValue object
+
+      return {
+        ...prev,
+        current: [...prev.current, newTypedValue],
+      };
+    });
   };
 
   useEffect(() => {
+    const orginal = textContent.split("");
+    const slicedOrginal = orginal.slice(0, typedValues.current.length);
+    const slicedTyped = typedValues.current.slice(0, typedValues.current.length)
 
-  },[typedValues])
+    const isSame = slicedTyped.every((typedValue, index) => {
+      return typedValue === slicedOrginal[index];
+    });
+  
+    console.log(isSame ? "Typed text matches original" : "Typed text does not match original");
+    console.log(slicedOrginal)
+    console.log(typedValues.current)
+  },[typedValues.current, textContent]);
+
   return (
     <div className="w-1/2 flex justify-center items-center relative z-0">
       <div className="w-full h-full text-3xl bg-transparent z-10 text_area p-6 absolute text-white"></div>
@@ -73,7 +74,12 @@ const TypingArea = () => {
         onChange={handleTyping}
         onBlur={handleFocus}
         ref={textAreaRef}
-     
+        onKeyDown={(e) => e.key === 'Backspace' && setTypedValues(prev => {
+          return {
+            ...prev,
+            current: prev.current.slice(0, prev.current.length - 1)
+          }
+        })}
       />
       <TextShade />
       <Blur isFocused={isFocused} handleFocus={handleFocus} />
